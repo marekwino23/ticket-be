5ded86416d48bc8cd20565852436ce3c54a0b8ed
Marek <marekw1996@gmail.com>
2020-04-13 15:45:17 +0200

    setup login done

--- index.js
+++ index.js
@@ -2,18 +2,86 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const db = require('./config/database');
 const handlers = require('./handlers');
 const app = express();
 const router = express.Router();
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET','PUT','POST','DELETE'],
+    credentials: true
+}));
 
+app.use(cookieParser());
 
-
-app.use(cors());
 app.use(express.json());
+
+app.post('/login', (req, res)  =>{
+    const crypto = req.body.password;
+    db.query(`SELECT * FROM users where email="${req.body.email}" AND password="${crypto}"`, function (error, results, fields) {
+        console.log('db login :', error, results.length, fields);
+        if(error || !results.length) res.status(401).json({ error: 'user does not exist '});
+        else {
+            const signOptions = {
+                expiresIn: '1d',
+              };
+              const CurrentDate = new Date();
+              CurrentDate.setMonth(CurrentDate.getMonth() + 1)
+            const { access_token, refresh_token } = generateTokens(req.body, signOptions);
+            console.log('cheers: '. access_token);
+            const cookieOptions = {
+                httpOnly: true,
+                expires: CurrentDate
+            };
+            res.cookie('access_token', access_token, cookieOptions)
+            res.cookie('refresh_token', refresh_token, { ...cookieOptions, expiresIn: '30d' });
+            res.status(200).json({ ok: true });
+        }
+    });
+});
+
 // router.use('*',handlers);
+
+let refreshTokens = []
+
+// app.post('/token', (req, res) => {
+//     const refresh_token = req.body.token
+//     if (refresh_token == null) return res.status(401)
+//     if (!refreshTokens.includes(refresh_token)) return res.status(403) 
+//     jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+//         if (err) return res.status(403)
+//         const tokens = generateTokens(req.body);
+//         res.json({...tokens});
+//     });
+// });
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+//     res.header("Access-Control-Allow-Headers", 'Content-Type');
+//     next();
+// });
+
+app.post('/verify', (req, res)  =>{
+    console.log('req headers: ', req.cookies);
+    jwt.verify(req.cookies.access_token, process.env.ACCESS_TOKEN_SECRET, (err,user) => { 
+        if (err) return res.status(401).json({ error: 'invalid token' });
+        return res.status(200).json({ error: '' });
+    })
+});
+
+app.post('/refresh', (req, res)  =>{
+    req.json(posts.filter(post => post.req.body === req.user.name))
+    const tokens = generateTokens(req.body);
+    console.log("success: ", process.env);
+    console.log('accessToken: ', accessToken);
+    res.setHeader('Set-Cookie', ['HttpOnly']);
+    res.json({accessToken, refreshToken })
+});
+
 app.get('/miejsca', authenticateToken, function(req, res) {
     let records;
     db.query('SELECT * FROM miejsca', function (error, results, fields) {
@@ -26,66 +94,14 @@ app.get('/miejsca', authenticateToken, function(req, res) {
     //   db.end();
 });
 
-let refreshTokens = []
-
-app.post('/token', (req, res) => {
-const refresh_token = req.body.token
-if (refreshToken == null) return res.sendStatus(401)
-if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403) 
-jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-if (err) return res.sendStatus(403)
-const tokens = generateTokens(req.body);
-res.json({...tokens});
- })
-})
-
-
-app.post('/login', (req, res)  =>{
-    const crypto = req.body.password;
-    db.query(`SELECT * FROM users where email="${req.body.email}" AND password="${crypto}"`, function (error, results, fields) {
-        console.log('db login :', error, results.length, fields);
-        if(error || !results.length) res.status(401).json({ error: 'user does not exist '});
-        else {
-            const tokens = generateTokens(req.body);
-    console.log('tokens: ', tokens);
-        res.setHeader('Set-Cookie', ['HttpOnly']);
-        res.json({...tokens});
-        }
-    });
-});
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", 'Content-Type');
-    next();
-})
-
-app.post('/verify', (req, res)  =>{
-    jwt.verify(req.cookies.access_token, process.env.ACCESS_TOKEN_SECRET, (err,user) => { 
-        if (err) return res.status(401).json({ error: 'invalid token' });
-        return res.status(200).json({ error: '' });
-    })
-});
-
-app.post('/refreshToken', (req, res)  =>{
-    //req.json(posts.filter(post => post.req.body === req.user.name))
-    const tokens = generateTokens(req.body);
-    console.log("success: ", process.env);
-    console.log('accessToken: ', accessToken);
-    res.setHeader('Set-Cookie', ['HttpOnly']);
-    res.json({accessToken, refreshToken })
-});
-
-
-function generateTokens(data) {
-    const access_token = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET);
-    const refresh_token = jwt.sign(data, process.env.REFRESH_TOKEN_SECRET);
-    refreshTokens.push(refresh_token)
+function generateTokens(data, options= {}) {
+    const access_token = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, options);
+    const refresh_token = jwt.sign(data, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d'});
+    refreshTokens.push(refresh_token);
     return { access_token, refresh_token };
 }
 
-function authenticateToken(req, res, nex){
+function authenticateToken(req, res, next){
     const authHeader = req.headers["authorization"]
     const token = authHeader && authHeader.split(' ')[1]
     if(token == null) return res.sendStatus(401)
@@ -94,10 +110,6 @@ function authenticateToken(req, res, nex){
     req.user = user
     next()
 })
-}
-
-function generateAccessToken(user){
-    return jwt.sign(req.body, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s'})  
 }
 
 app.listen(8000, function() {
