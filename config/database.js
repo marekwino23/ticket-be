a54d087b922d6c7b245257a21b6085a9ea218412
Marek <marekw1996@gmail.com>
2020-04-23 16:31:52 +0200

    added err object from db connection

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ var db = mysql.createConnection({
  
 db.connect(function(err){
   if(err){
-    console.log('Error connecting to Db');
+    console.log('Error connecting to Db: ', err);
     return;
   }
   console.log('Connection established');
